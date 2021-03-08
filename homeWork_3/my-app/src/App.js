8eef85114f97453bfd8376bc60ece50ce7a27c4a
Tenebran <67394588+Tenebran@users.noreply.github.com>
2021-03-08 04:08:19 +0100

    Delete homeWork_3/my-app directory

--- homeWork_3/my-app/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import Todos from './modules/todos/components/Todos/Todos';
-
-export default class App extends Component {
-  render() {
-    return <Todos />;
-  }
-}
--- homeWork_3/my-app/src/modules/contacts/constans.js
+++
@@ -1,1 +0,0 @@
-export const CONTACTS_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/contacts/';
