cae97bb991a3f67fd0f4ef26f63e0d6407390c4f
Tenebran <67394588+Tenebran@users.noreply.github.com>
2021-03-08 03:51:15 +0100

    Add files via upload

---
+++ homeWork_3/my-app/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Todos from './modules/todos/components/Todos/Todos';
+import './App.scss';
+
+export default class App extends Component {
+  render() {
+    return <Todos />;
+  }
+}
