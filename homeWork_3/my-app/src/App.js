0be68cb359f409b8feb76a8eb1c6fce61b2b06aa
Tenebran <67394588+Tenebran@users.noreply.github.com>
2021-03-08 03:37:15 +0100

    Delete homeWork_3 directory

--- homeWork_3/my-app/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import Todos from './modules/todos/components/Todos/Todos';
-import './App.scss';
-
-export default class App extends Component {
-  render() {
-    return <Todos />;
-  }
-}
