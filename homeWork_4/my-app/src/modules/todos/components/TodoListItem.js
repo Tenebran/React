5a9647bc08ba12e4b6085d6e5b269a417f444f38
Tenebran <67394588+Tenebran@users.noreply.github.com>
2021-03-15 04:49:41 +0100

    Add files via upload

---
+++ homeWork_4/my-app/src/modules/todos/components/TodoListItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default function TodoListItem({ item, onToggle, onDelete }) {
+  function onItemClick() {
+    onToggle(item.id);
+  }
+
+  function onDeleteBtnClick(e) {
+    e.stopPropagation();
+
+    onDelete(item.id);
+  }
+
+  return (
+    <li onClick={onItemClick} style={getItemStyle(item)}>
+      {item.title}
+      <span onClick={onDeleteBtnClick}>X</span>{' '}
+    </li>
+  );
+
+  function getItemStyle({ completed }) {
+    return {
+      cursor: 'pointer',
+      backgroundColor: completed ? 'green' : 'yellow',
+    };
+  }
+}
