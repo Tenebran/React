6af68dbb479d330f30e527d6c11cd35d00059c4e
Tenebran <67394588+Tenebran@users.noreply.github.com>
2021-03-15 05:07:09 +0100

    Add files via upload

---
+++ homeWork_4/my-app/src/modules/todos/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TodoListItem from './TodoListItem';
+
+export default function TodoList({ list, onToggle, onDelete }) {
+  return (
+    <ul>
+      {list.map(item => (
+        <TodoListItem key={item.id} item={item} onToggle={onToggle} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+}
